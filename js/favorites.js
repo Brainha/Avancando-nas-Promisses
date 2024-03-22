1a7dded8b8b3547cee4299f6ade6e978fc0ef204
Braian <braiannickolas12@gmail.com>
2024-03-22 08:34:25 -0300

    Aula 240
    Imutabilidade para trabalhar com os dado da aplicacao

--- js/favorites.js
+++ js/favorites.js
@@ -1,26 +1,90 @@
 export class Favorites {
   constructor(root) {
-    this.root = document.querySelector(root)
+    this.root = document.querySelector(root);
+    this.load()
+  }
+
+  load() {
+    this.entries = [
+      {
+        login: 'maykbrito',
+        name: "Mayk Brito",
+        public_repos: '76',
+        followers: '120000'
+      },
+      {
+        login: 'diego3g',
+        name: "Diego Fernandes",
+        public_repos: '76',
+        followers: '120000'
+      }
+    ]
+
   }
 }
+
+
 
 export class FavoritesView extends Favorites {
   constructor(root) {
     super(root)
+
+    this.tbody = this.root.querySelector('table tbody')
 
     this.update()
   }
 
   update() {
     this.removeAllTr()
+
+    this.entries.forEach(user => {
+      const row = this.createRow()
+
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user a').href = `https://github.com/${user.login}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositories').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+
+
+      this.tbody.append(row)
+    })
+
   }
 
-  removeAllTr() {
-    const tbody = this.root.querySelector('table tbody')
+  createRow() {
+    const tr = document.createElement("tr");
 
-    tbody.querySelectorAll('tr').forEach((tr) => {
-      tr.remove()
-    })
+    tr.innerHTML = `
+    <td class="user">
+      <img
+        src=""
+        alt=""
+      />
+      <a href="" target="_blank">
+        <p></p>
+        <span></span>
+      </a>
+    </td>
+    <td class="repositories"></td>
+    <td class="followers"></td>
+    <td>
+      <button class="remove">&times;</button>
+    </td>
+    `
+
+    return tr
+
+
+  }
+  removeAllTr() {
+    this.tbody.querySelectorAll('tr')
+
+      .forEach((tr) => {
+        tr.remove()
+      })
   }
 
 }
